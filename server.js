f66d51e907c68b1d9c3ce0c377b51f6aa2fab0c1
genstubo <137889448+genstubo@users.noreply.github.com>
2023-11-06 23:39:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('./config/db');
 
 const app = require('express')();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const userRouter = require('./api/User');
 
@@ -12,6 +12,6 @@ app.use(bodyPraser());
 
 app.use('/user', userRouter)
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${port}`);
 })
